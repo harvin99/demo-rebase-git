a22581a0c41733c7631bf368879cfffa414df645
Hung Tran <57244326+harvin99@users.noreply.github.com>
2024-07-04 09:14:53 +0700

    Update index.js - remove func 6 7

--- index.js
+++ index.js
@@ -7,8 +7,3 @@ export const demoFunc3 = () => {}
 export const demoFunc4 = (a, b) => {return a + b}
 
 export const demoFunc5 = () => {}
-
-export const demoFunc6 = () => {}
-export const demoFunc7 = () => {
-    return;
-}
