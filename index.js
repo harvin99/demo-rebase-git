82255425dd87ef54a0be7a0fb4b489a623ed9488
harvin99 <hungtx1001@gmail.com>
2024-07-04 09:15:37 +0700

    add params func 6

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ export const demoFunc3 = () => {}
 export const demoFunc4 = (a, b) => {return a + b}
 
 export const demoFunc5 = () => {}
+
+export const demoFunc6 = (params) => { console.log(params)}
+
+export const demoFunc7 = () => {
+    return;
+}
+
+
